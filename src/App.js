8f4d1de860a75ff644dfc852e8c00028c3a3ebca
giaphoang <hhgiap241>
2022-07-07 17:06:46 +0700

    add scroll to top button and clean up function in useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
 import './App.css';
 import PostItem from "./PostItem";
+import {useEffect, useState} from "react";
 
 
 // const courses = [
@@ -19,9 +19,40 @@ import PostItem from "./PostItem";
 // ];
 
 function App() {
+    const [showGoToTop, setShowGoToTop] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 200) {
+                setShowGoToTop(true);
+            } else {
+                setShowGoToTop(false);
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
+    const goToTopHandler = () => {
+        window.scrollTo(0, 0);
+        setShowGoToTop(false);
+    }
     return (
         <div className="App">
             <PostItem/>
+            {
+                showGoToTop && (
+                    <button
+                        style={{
+                            position: 'fixed',
+                            right: 20,
+                            bottom: 20
+                        }}
+                        onClick={goToTopHandler}
+                    >Go to top
+                    </button>
+                )
+            }
         </div>
     );
 }
