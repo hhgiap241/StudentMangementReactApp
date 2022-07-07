ffc27b1f16a573b306eb88a99b77245ad221d2d5
giaphoang <hhgiap241>
2022-07-07 16:21:32 +0700

    call api with useEffect has dependency

--- src/PostItem.js
+++ src/PostItem.js
@@ -2,18 +2,33 @@ import React, {useEffect, useState} from "react";
 
 const PostItem = () => {
     const [posts, setPosts] = useState([])
-    useEffect(() =>{
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(posts => setPosts(posts));
-    }, [])
+    const types = ['posts', 'comments', 'albums'];
+    const [type, setType] = useState('posts')
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+            .then(response => response.json())
+            .then(posts => setPosts(posts));
+    }, [type])
 
     return (
         <div>
+            {
+                types.map(item => {
+                    return (
+                        <button
+                            key={item}
+                            style={type === item ? {color: '#fff', backgroundColor: '#333'} : {}}
+                            onClick={() => setType(item)}
+                        >
+                            {item}
+                        </button>
+                    )
+                })
+            }
             <ul>
                 {
                     posts.map(post => {
-                        return <li key={post.id}>{post.title}</li>
+                        return <li key={post.id}>{post.title || post.name}</li>
                     })
                 }
             </ul>
